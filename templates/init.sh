99a82d1dbe955e24a7ea8611d6c19fc4e9f2f592
Kenta Murata <mrkn@mrkn.jp>
2017-06-06 17:57:19 +0900

    Update init.sh

--- templates/init.sh
+++ templates/init.sh
@@ -15,5 +15,12 @@ Defaults:%wheel env_keep += SSH_AUTH_SOCK
 SUDO
 
 apt-get update -y
-apt-get install -y ssh sudo
+apt-get install -y curl ssh sudo git-core
 service ssh start
+
+mkdir -p /usr/local/sbin
+curl -o /usr/local/sbin/mitamae https://github.com/itamae-kitchen/mitamae/releases/download/v1.4.5/mitamae-x86_64-linux
+chmod 755 /usr/local/sbin/mitamae
+
+git clone https://github.com/speee/eng-security-bootcamp-infra.git /home/infra/eng-security-bootcamp-infra
+chown -R infra:infra /home/infra/eng-security-bootcamp-infra
