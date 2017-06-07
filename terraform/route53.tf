c29294757d1c145043acbaacbfcbfd12b59fb540
Kenta Murata <mrkn@mrkn.jp>
2017-06-07 11:42:40 +0900

    Add speee-sbc.mrkn.jp zone

---
+++ terraform/route53.tf
@@ -0,0 +1,15 @@
+resource "aws_route53_zone" "primary" {
+  name = "speee-sbc.mrkn.jp"
+}
+
+output "primary_zone_nameservers" {
+  value = ["${aws_route53_zone.primary.name_servers}"]
+}
+
+resource "aws_route53_record" "bastion" {
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name = "bastion"
+  type = "A"
+  ttl = "60"
+  records = ["${aws_eip.bastion.public_ip}"]
+}
