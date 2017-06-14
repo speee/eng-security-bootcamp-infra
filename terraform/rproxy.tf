9ab1e6d3ebd7180e6c42177f15c2317b8c3224f0
Kenta Murata <mrkn@mrkn.jp>
2017-06-14 16:03:46 +0900

    Fix rproxy security group
    - Reject incoming to 80 port
    - Accept only from Speee's IP addresses to 443 port

--- terraform/rproxy.tf
+++ terraform/rproxy.tf
@@ -32,21 +32,13 @@ resource "aws_security_group" "rproxy" {
 
   ingress {
     protocol  = "tcp"
-    from_port = 80
-    to_port   = 80
-    cidr_blocks = [
-      "0.0.0.0/0"
-      # from Speee (disabled)
-      # "122.212.158.58/32",
-      # "124.35.147.130/32"
-    ]
-  }
-
-  ingress {
-    protocol  = "tcp"
     from_port = 443
     to_port   = 443
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      # from Speee
+      "122.212.158.58/32",
+      "124.35.147.130/32"
+    ]
   }
 
   egress {
