523b8f51dd87b6b478323f7edcbc786c195a9a07
Kenta Murata <mrkn@mrkn.jp>
2017-06-30 11:54:42 +0900

    Introduce branch parameter for cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,5 +3,6 @@ lock "3.8.2"
 
 set :application, "speee-sbc-infra"
 set :repo_url, "git@github.com:speee/eng-security-bootcamp-infra.git"
+set :branch, ENV['branch'] || 'master'
 set :deploy_to, "/home/infra/speee-sbc-infra"
 set :pty, true
