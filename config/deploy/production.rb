c2947ffca200962674dc643342ed18293e716f72
Kenta Murata <mrkn@mrkn.jp>
2017-07-03 21:55:16 +0900

    Specify private key file of infra user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+set :ssh_options, keys: [File.expand_path("../id_rsa.infra")]
+
 server "bastion.speee-sbc", user: "infra", roles: %w(bastion)
 server "rproxy.speee-sbc", user: "infra", roles: %w(rproxy)
 server "app-000.speee-sbc", user: "infra", roles: %w(app)
